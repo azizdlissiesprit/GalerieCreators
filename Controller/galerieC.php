d805786393a01ba028da297d50d2250b888bba18
zeinebkharrat <131543861+zeinebkharrat@users.noreply.github.com>
2023-12-05 10:11:32 +0100

    Add files via upload

--- Controller/galerieC.php
+++ Controller/galerieC.php
@@ -1,7 +1,135 @@
 <?php
 require_once(__DIR__ . '/../config.php');
 class GalerieC{
-    public function list_Galerie(){
+    function showlink_jointure($id_gal){
+        $sql = "SELECT * from links where idGalerie = :idG";
+        $db = configurer::getConnexion();
+        try {
+            $query = $db->prepare($sql);
+            $query->bindValue(':idG', $id_gal);
+            $query->execute();
+            $Event = $query->fetch();
+            return $Event;
+        } catch (Exception $e) {
+            die('Error: ' . $e->getMessage());
+        }
+    }
+    function last_id_inserted(){
+        $req = "SELECT idGalerie FROM galeries ORDER BY idGalerie DESC LIMIT 1";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            $lastId = $list->fetch(PDO::FETCH_ASSOC);
+            return $lastId['idGalerie'];
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    function list_liens(){
+        $req = "SELECT * FROM links";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function jointure($link){
+        $sql = "SELECT * FROM galeries
+        JOIN links ON galeries.idGalerie = links.idGalerie
+        where links.link LIKE :link";
+
+        $db = configurer::getConnexion();
+        try {
+            $req = $db->prepare($sql);
+            $req->bindParam(':link', $link);
+            $req->execute();
+            $list = $req->fetchAll(PDO::FETCH_ASSOC);
+            return $list;
+        } catch (Exception $e) {
+            die('Error:' . $e->getMessage());
+        }
+    }
+    function rechercher($valeur){
+        $sql = "SELECT * FROM galeries
+        WHERE nomGalerie LIKE :valeur_one
+        OR proprieteGalerie LIKE :valeur_two
+        OR lieuGalerie LIKE :valeur_three";
+        $db = configurer::getConnexion();
+        try {
+            $query = $db->prepare($sql);
+            $query->bindValue(':valeur_one', '%' . $valeur . '%');
+            $query->bindValue(':valeur_two', '%' . $valeur . '%');
+            $query->bindValue(':valeur_three', '%' . $valeur . '%');
+            $query->execute();
+            $events = $query->fetchAll();
+            return $events;
+        } catch (Exception $e) {
+            die('Error: ' . $e->getMessage());
+        }
+    }
+    public function filter_nomGal_ASC(){
+        $req = "SELECT * FROM galeries ORDER BY nomGalerie ASC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function filter_nomGal_DESC(){
+        $req = "SELECT * FROM galeries ORDER BY nomGalerie DESC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function filter_proprieteGal_ASC(){
+        $req = "SELECT * FROM galeries ORDER BY proprieteGalerie ASC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function filter_proprieteGal_DESC(){
+        $req = "SELECT * FROM galeries ORDER BY proprieteGalerie DESC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function filter_lieuGal_ASC(){
+        $req = "SELECT * FROM galeries ORDER BY lieuGalerie ASC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    public function filter_lieuGal_DESC(){
+        $req = "SELECT * FROM galeries ORDER BY lieuGalerie DESC";
+        $db = configurer::getConnexion();
+        try{
+            $list = $db->query($req);
+            return $list;
+        } catch (Exception $e){
+            die('Error:' . $e->getMessage());
+        }
+    }
+    function list_Galerie(){
         $req = "SELECT * FROM galeries";
         $db = configurer::getConnexion();
         try{
@@ -25,14 +153,16 @@ class GalerieC{
 
     function addGalerie($galerie){
         $sql = "INSERT INTO galeries
-        VALUES (null, :nom_gal, :propriete_gal, :lieu_gal)";
+        VALUES (null, :nom_gal, :propriete_gal, :lieu_gal, :img_gal)";
         $db = configurer::getConnexion();
         try {
             $query = $db->prepare($sql);
             $query->execute([
                 'nom_gal'  => $galerie->getNomGalerie(),
-                'propriete_gal' => $galerie->GetProprieteGalerie() , 
-                'lieu_gal' => $galerie->get_lieuGalerie()
+                'propriete_gal' => $galerie->GetProprieteGalerie(),
+                'lieu_gal' => $galerie->get_lieuGalerie(),
+                'img_gal' => $galerie->get_imgGalerie()
+
             ]);
         } catch (Exception $e) {
             echo 'Error: ' . $e->getMessage();
@@ -58,7 +188,8 @@ class GalerieC{
                 'UPDATE galeries SET
                     nomGalerie  =       :nom_galerie,
                     proprieteGalerie =  :propriete_galerie,
-                    lieuGalerie =       :lieu_galerie
+                    lieuGalerie =       :lieu_galerie,
+                    imgGalerie =        :img_galerie
                 WHERE idGalerie =       :id_galerie'
             );
             $query->execute([
@@ -66,11 +197,52 @@ class GalerieC{
                 'nom_galerie' => $galerie->getNomGalerie(),
                 'propriete_galerie' => $galerie->GetProprieteGalerie(),
                 'lieu_galerie' => $galerie->get_lieuGalerie(),
+                'img_galerie' => $galerie->get_imgGalerie()
             ]);
-            echo $query->rowCount() . " records UPDATED successfully <br>";
+            echo $query->rowCount() . " Galerie records UPDATED successfully <br>";
         } catch (PDOException $e) {
             $e->getMessage();
         }
     }
+}
+
+class linkC{
+    function ajouter_lien($lien){
+        $sql = "INSERT INTO links
+        VALUES (null, :idGalerie, :link)";
+        $db = configurer::getConnexion();
+        try {
+            $query = $db->prepare($sql);
+            $query->execute([
+                'idGalerie'  => $lien->getIdGalerie(),
+                'link' => $lien->getLinkGalerie()
+            ]);
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+    function updatelink($id_link, $id_galerie, $new_link){
+        try {
+            $db = configurer::getConnexion();
+            $query = $db->prepare(
+                'UPDATE links SET
+                    idGalerie = :id_galerie,
+                    link  = :link_updated
+                WHERE id_link = :id_link'
+            );
+    
+            $query->execute([
+                'id_galerie' => $id_galerie,
+                'link_updated' => $new_link,
+                'id_link' => $id_link,
+            ]);
+    
+            echo $query->rowCount() . " records UPDATED successfully <br>";
+        } catch (PDOException $e) {
+            // Output the error message
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+    
 }
 ?>
