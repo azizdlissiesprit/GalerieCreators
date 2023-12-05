877522432ffa5684466d656025a6c3426ed828df
zeinebkharrat <131543861+zeinebkharrat@users.noreply.github.com>
2023-12-05 10:12:27 +0100

    Add files via upload

--- Model/Galerie_Model.php
+++ Model/Galerie_Model.php
@@ -4,12 +4,15 @@ class Galerie{
     public string $nom_galerie;
     public string $propriete_galerie;
     public string $lieu_galerie;
+    public ?string $img_galerie = null;
+
     /*-----------------------  constructor of "Galerie" ---------------------------*/
-    public function __construct($id_gal, $nom_gal, $propriete_gal, $lieu_gal){
+    public function __construct($id_gal, $nom_gal, $propriete_gal, $lieu_gal, $img_gal){
         $this->id_galerie = $id_gal;
         $this->nom_galerie = $nom_gal;
         $this->propriete_galerie = $propriete_gal;
         $this->lieu_galerie = $lieu_gal;
+        $this->img_galerie = $img_gal;
     }
     public function getIdGalerie(){
         return $this->id_galerie;
@@ -35,5 +38,41 @@ class Galerie{
    public function get_lieuGalerie(){
        return $this->lieu_galerie;
    }
+   public function set_imgGalerie($new_img){
+        $this->img_galerie = $new_img;
+        return $this;
+    }
+    public function get_imgGalerie(){
+        return $this->img_galerie;
+    }
+}
+
+class link_galerie{
+    public ?int $id_link = null;
+    public int $id_galerie;
+    public string $link_galerie;
+
+    public function __construct($id_lien, $id_gal, $link_gal){
+        $this->id_link = $id_lien;
+        $this->id_galerie = $id_gal;
+        $this->link_galerie = $link_gal;
+    }
+    public function getIdLink(){
+        return $this->id_link;
+    }
+    public function getIdGalerie(){
+        return $this->id_galerie;
+    }
+    public function setIdGalerie($new_id_gal){
+        $this->id_galerie = $new_id_gal;
+        return $this;
+    }
+    public function getLinkGalerie(){
+        return $this->link_galerie;
+    }
+    public function setLinkGalerie($new_link_gal){
+        $this->link_galerie = $new_link_gal;
+        return $this;
+    }
 }
 ?>
