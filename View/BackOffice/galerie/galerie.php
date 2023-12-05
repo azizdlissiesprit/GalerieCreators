0daa4f294c6244b07b3a0e483618e12e7ef0b64f
zeinebkharrat <131543861+zeinebkharrat@users.noreply.github.com>
2023-12-05 10:13:11 +0100

    Add files via upload

--- View/BackOffice/galerie/galerie.php
+++ View/BackOffice/galerie/galerie.php
@@ -2,22 +2,64 @@
 include "../../../Controller/galerieC.php";
 $c = new GalerieC();
 $tab = $c->list_Galerie();
+
+
+$filtre ="Selectionner un filtre";
+if (isset($_POST['filtre_boutton'])) {
+    if (isset($_POST['filtre'])) {
+        $filtre = $_POST['filtre'];
+    }
+    if($filtre === "Selectionner un filtre") {
+        $tab = $c->list_Galerie();
+    }
+    else if ($filtre === 'Nom galerie Asc') {
+        $tab = $c->filter_nomGal_ASC();
+    } 
+    else if ($filtre === 'Nom galerie Desc') {
+        $tab = $c->filter_nomGal_DESC();
+    }
+    else if($filtre === "Propriete Asc") {
+        $tab = $c->filter_proprieteGal_ASC();
+    }
+    else if($filtre === "Propriete Desc") {
+        $tab = $c->filter_proprieteGal_DESC();
+    }
+    else if($filtre === "lieu galerie Asc") {
+        $tab = $c->filter_lieuGal_ASC();
+    }
+    else if($filtre === "lieu galerie Desc") {
+        $tab = $c->filter_lieuGal_DESC();
+    }
+}
+
+// recherche
+if (isset($_POST["submit_btn_recherche"])){
+    $recherche = $_POST["recherche"];
+    $tab = $c->rechercher($recherche);
+}
+
+// link
+$liens = $c->list_liens();
+if (isset($_POST["submit_jointure"])) {
+    $link = $_POST["lien"];
+    $tab = $c->jointure($link);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="../style.css">
     <title>Evenement Administration</title>
 </head>
-
 <body>
-
     <div class="container">
         <!-- Sidebar Section -->
         <aside>
@@ -88,9 +130,48 @@ $tab = $c->list_Galerie();
             </div>
         </aside>
         <!-- End of Sidebar Section -->
-
         <!-- Main Content -->
         <main>
+            <div class="content">
+                <nav>
+                    <form action="" method="POST">
+                        <div class="form-input">
+                            <input type="search" name="recherche" autocomplete="off" placeholder="rechercher">
+                            <button type="submit" class="search-btn" name="submit_btn_recherche" ><i class='bx bx-search'></i></button>
+                        </div>
+                    </form>
+
+            <form action="" method="POST">
+                <select name="lien">
+                <?php
+                        foreach ($liens as $lien) {
+                            echo "<option value='" . $lien["link"] . "'>" . $lien["link"] . "</option>";
+                        }
+                    ?>
+                </select>
+                <input type="submit" name="submit_jointure" value="afficher">
+            </form>
+
+
+                    <!-- <<=========================  filter ==================================>> -->
+                    <div class="select-container">
+                        <form action="" method="POST">
+                            <select class="select-box" name="filtre" >
+                                <option value="Selectionner un filtre">Selectionner un filtre</option>
+                                <option value="Nom galerie Asc">Nom galerie Asc</option>
+                                <option value="Nom galerie Desc">Nom galerie Desc</option>
+                                <option value="Propriete Asc">Propriete Asc</option>
+                                <option value="Propriete Desc">Propriete Desc</option>
+                                <option value="lieu galerie Asc">lieu galerie Asc</option>
+                                <option value="lieu galerie Desc">lieu galerie Desc</option>
+                            </select>
+                            <input type="submit" class="appliquer" name="filtre_boutton" value="filtrer">
+                        </form>
+                    </div>
+                    <!-- <<=========================  filter fin ==================================>> -->
+
+                </nav>
+            </div>
             <h1>Galeries</h1>
             <div class="new-users">
                 <div class="user-list" id="adding_galeries_key">
@@ -110,6 +191,7 @@ $tab = $c->list_Galerie();
                             <th>Nom Galerie</th>
                             <th>propriete Galerie</th>
                             <th>lieu Galerie</th>
+                            <th>image Galerie</th>
                             <th>Update</th>
                             <th>Delete</th>
                         </tr>
@@ -121,6 +203,7 @@ $tab = $c->list_Galerie();
                                 <td><h2><?= $galerie['nomGalerie']; ?></h2></td>
                                 <td><h2><?= $galerie['proprieteGalerie']; ?></h2></td>
                                 <td><h2><?= $galerie['lieuGalerie']; ?></h2></td>
+                                <td><img class="images_table" src="uploads/<?= $galerie['imgGalerie'] ?>"></td>
                                 <td><h1><a href="updateGalerie.php?idGalerie=<?php echo $galerie['idGalerie'];?>">Update</a></h1></td>
                                 <td><h1><a href="deleteGalerie.php?idGalerie=<?php echo $galerie['idGalerie'];?>">Delete</a></h1></td>
                             </tr>
@@ -130,21 +213,31 @@ $tab = $c->list_Galerie();
             </div>
             <!-- End Users Table -->
         <div class="new-galerie-form" id="formulaire-new-galerie" style="display: none;">
-            <form action="addGalerie.php" method="POST">
+            <form action="addGalerie.php" method="POST"  enctype="multipart/form-data">
                 <table>
                     <tr>
-                        <td><label for="nom_galerie">Nom :</label></td>
+                        <td><label for="nom_galerie">Nom </label></td>
                         <td><input type="text" id="nom_galerie" name="nom_galerie" autocomplete="off"/></td>
                     </tr>
                     <tr>
-                        <td><label for="propriete_galerie">propriete :</label></td>
+                        <td><label for="propriete_galerie">propriete </label></td>
                         <td><input type="text" id="propriete_galerie" name="propriete_galerie" autocomplete="off"/></td>
                     </tr>
                     <tr>
-                        <td><label for="lieu_galerie">lieu :</label></td>
+                        <td><label for="lieu_galerie">lieu </label></td>
                         <td><input type="text" id="lieu_galerie" name="lieu_galerie" autocomplete="off"/></td>
                     </tr>
-                    <td><input type="submit" value="Save"></td>
+                    <tr>
+                        <td><label for="img_galerie">image</label></td>
+                        <td><input type="file" required id="img_galerie" name="img_galerie"/></td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="lien_galerie">lien site web </label></td>
+                        <td><input type="text" id="lien_galerie" name="lien_galerie" autocomplete="off"/></td>
+                    </tr>
+
+                    <td><input type="submit" value="Save" onclick="return valider_formulaire_galerie()"></td>
                     <td><input type="reset" value="Reset"></td>
                     <td><input type="button" value="cancel" onclick="hide_formulaire_galerie()"></td>
                 </table>
@@ -183,13 +276,6 @@ $tab = $c->list_Galerie();
             </div>
             <!-- End of Nav -->
 
-            <div class="user-profile">
-                <div class="logo">
-                    <img src="../images/logo.png">
-                    <h2>GalerieCreators</h2>
-                    <p>Best Fullstack Web Developer Team</p>
-                </div>
-            </div>
         </div>
 
 
@@ -198,6 +284,38 @@ $tab = $c->list_Galerie();
     <script src="../index.js"></script>
 </body>
 <script>
+
+    function is_word(ch) {
+        var allowedChars = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (var i = 0; i < ch.length; i++) {
+            if (allowedChars.indexOf(ch[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function valider_formulaire_galerie(){
+        nom = document.getElementById("nom_galerie").value;
+        propriete = document.getElementById("propriete_galerie").value;
+        lieu = document.getElementById("lieu_galerie").value;
+
+        if (!nom || !is_word(nom) || nom.length > 20) {
+            alert("le Nom doit etre une chaine alphabetique de longueur inferieur a 20");
+            return false;
+        }
+
+        if (!propriete || !is_word(propriete) || propriete.length > 20) {
+            alert("propriete doit etre une chaine alphabetique de longueur inferieur a 20");
+            return false;
+        }
+        
+        if (!lieu || lieu.length > 20) {
+            alert("lieu doit etre de longueur inferieur a 20");
+            return false;
+        }
+
+    }
     function hide_formulaire_galerie(){
         form = document.getElementById("formulaire-new-galerie");
         form.style.display = "none";
--- View/BackOffice/galerie/updateGalerie.php
+++ View/BackOffice/galerie/updateGalerie.php
@@ -3,22 +3,42 @@ include "../../../Controller/galerieC.php";
 include "../../../Model/Galerie_Model.php";
 $galerie = null;
 $galerieC = new GalerieC();
-if (isset($_POST["nom_galerie"]) && isset($_POST["propriete_galerie"]) && isset($_POST["lieu_galerie"])){
+$linkc = new linkC();
+$old_lien =null;
+if (isset($_POST["nom_galerie"]) 
+    && isset($_POST["propriete_galerie"]) 
+    && isset($_POST["lieu_galerie"])
+    && isset($_POST["lien_galerie"])
+    && ($_FILES['img_galerie']['name']!="")){
+
         $nom_gal = $_POST["nom_galerie"];
         $date_gal = $_POST["propriete_galerie"];
         $lieu_gal = $_POST["lieu_galerie"];
-        if (!empty($nom_gal) &&!empty($date_gal) &&!empty($lieu_gal)){
-            $galerie = new Galerie(null, $nom_gal, $date_gal, $lieu_gal);
-            var_dump($galerie);
-            $galerieC->updateGalerie($galerie, $_GET['idGalerie']);
-            header('Location:galerie.php');
-    }
+        $new_link = $_POST["lien_galerie"];
+        // <<============= debut image ===================== >>
+        $img_name = $_FILES['img_galerie']['name']; // nom d'image
+        $tmp_name = $_FILES['img_galerie']['tmp_name']; // 
+        // <<============= fin image ===================== >>
+        move_uploaded_file($tmp_name, 'uploads/'.$img_name);
+
+        $galerie = new Galerie(null, $nom_gal, $date_gal, $lieu_gal,$img_name);
+        
+        
+        $id_link_tab = $galerieC->showlink_jointure($_GET['idGalerie']);
+        $id_gal =$_GET['idGalerie'];
+        $id_link = $id_link_tab["id_link"];
+        $linkc->updatelink($id_link,$id_gal,$new_link);
+        $galerieC->updateGalerie($galerie, $_GET['idGalerie']);
+
+        header('Location:galerie.php');
+    
 }
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>User Display</title>
 </head>
 
@@ -26,49 +46,148 @@ if (isset($_POST["nom_galerie"]) && isset($_POST["propriete_galerie"]) && isset(
     <?php
     if (isset($_GET['idGalerie'])) {
         $old_gal = $galerieC->showGalerie($_GET['idGalerie']);
+        $old_lien = $galerieC->showlink_jointure($_GET['idGalerie']);
     ?>
-    <form action="" method="POST">
-        <table>
-        <tr>
-            <td><label for="idGalerie">Id galerie :</label></td>
-            <td>
-                <input type="text" id="idGalerie" name="idGalerie" 
-                value="<?php echo $_GET['idGalerie'] ?>" readonly/>
-            </td>
-            </tr>
+    <div class="update-galerie-formulaire">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table>
             <tr>
-                <td><label for="nom_galerie">Nom galerie :</label></td>
+                <td><label for="idGalerie">Id galerie</label></td>
                 <td>
-                    <input type="text" id="nom_galerie" name="nom_galerie" 
-                    value="<?php echo $old_gal['nomGalerie']?>"/>
+                    <input type="text" id="idGalerie" name="idGalerie" 
+                    value="<?php echo $_GET['idGalerie'] ?>" readonly/>
                 </td>
-            </tr>
-            <tr>
-                <td><label for="propriete_galerie">propriete galerie :</label></td>
-                <td>
-                    <input type="text" id="propriete_galerie" name="propriete_galerie"
-                     value="<?php echo $old_gal['proprieteGalerie']?>"/>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="lieu_galerie">lieu galerie:</label></td>
-                <td>
-                    <input type="text" id="lieu_galerie" name="lieu_galerie"
-                     value="<?php echo $old_gal['lieuGalerie']?>"/>
-                </td>
-            </tr>
-            <td>
-                <input type="submit" value="Save">
-            </td>
-            <td>
-                <input type="reset" value="Reset">
-            </td>
-        </table>
+                </tr>
+                <tr>
+                    <td><label for="nom_galerie">Nom galerie</label></td>
+                    <td>
+                        <input type="text" id="nom_galerie" name="nom_galerie" 
+                        value="<?php echo $old_gal['nomGalerie']?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="propriete_galerie">propriete galerie</label></td>
+                    <td>
+                        <input type="text" id="propriete_galerie" name="propriete_galerie"
+                        value="<?php echo $old_gal['proprieteGalerie']?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="lieu_galerie">lieu galerie</label></td>
+                    <td>
+                        <input type="text" id="lieu_galerie" name="lieu_galerie"
+                        value="<?php echo $old_gal['lieuGalerie']?>"/>
+                    </td>
+                </tr>
 
-    </form>
+                <tr>
+                    <td><label for="img_galerie">image galerie</label></td>
+                    <td>
+                        <img class="images_table" src="uploads/<?= $old_gal['imgGalerie'] ?>">
+                        <input type="file" required id="img_galerie" name="img_galerie"
+                        value="<?php echo $old_gal['imgGalerie']?>"/>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td><label for="lien_galerie">lien site web</label></td>
+                    <td>
+                        <input type="text" id="lien_galerie" name="lien_galerie"
+                        value="<?php echo $old_lien["link"]?>"/>
+                    </td>
+                </tr>
+
+                <td>
+                    <input type="submit" value="Save" onclick="return valider_formulaire_update_galerie()">
+                </td>
+                <td>
+                    <input type="reset" value="Reset">
+                </td>
+                <td>
+                    <a href="./galerie.php">Cancel</a>
+                </td>
+            </table>
+
+        </form>
+    </div>
     <?php
     }
     ?>
 </body>
+<script>
+    function is_word(ch) {
+        var allowedChars = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (var i = 0; i < ch.length; i++) {
+            if (allowedChars.indexOf(ch[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    function valider_formulaire_update_galerie(){
+        nom = document.getElementById("nom_galerie").value;
+        propriete = document.getElementById("propriete_galerie").value;
+        lieu = document.getElementById("lieu_galerie").value;
+
+        if (!nom || !is_word(nom) || nom.length > 20) {
+            alert("le Nom doit etre une chaine alphabetique de longueur inferieur a 20");
+            return false;
+        }
+
+        if (!propriete || !is_word(propriete) || propriete.length > 20) {
+            alert("propriete doit etre une chaine alphabetique de longueur inferieur a 20");
+            return false;
+        }
+        
+        if (!lieu || lieu.length > 20) {
+            alert("lieu doit etre de longueur inferieur a 20");
+            return false;
+        }
+
+    }
+</script>
+
+<style>
+    
+.update-galerie-formulaire{
+    background-color: var(--color-white);
+    padding: var(--card-padding);
+    border-radius: var(--card-border-radius);
+    box-shadow: var(--box-shadow);
+    width: 70%;
+    top: 10vh;
+    left: 15%;
+    position: absolute;
+}
+.update-galerie-formulaire label{
+    font-size: 2rem;
+}
+
+.update-galerie-formulaire input[type="text"]{
+    font-size: 3rem;
+    color: var(--color-primary);
+    background-color: transparent;
+    border: 2px solid #34a83c;
+    cursor: text;
+    padding: 15px;
+    border-radius: 10px;
+}
+
+.update-galerie-formulaire input[type="submit"], input[type="reset"],a{
+    padding: 10px;
+    border: 2px solid #34a83c;
+    border-radius: 10px;
+    font-size: 3rem;
+    color: #34a83c;
+    cursor: pointer;
+    background-color: transparent;
+}
+
+.update-galerie-formulaire input[type="submit"]:hover, input[type="reset"]:hover,a:hover{
+    color: white;
+    background-color: #34a83c;
+}
+</style>
 </html>
